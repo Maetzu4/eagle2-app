35dad34f0cdc1067af38100b9f520d3410c86b5f
Nate <6814natsu@gmail.com>
2025-03-16 11:03:54 -0500

    ver 9 deploy

--- src/app/digitador/page.client.tsx
+++ src/app/digitador/page.client.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import LogOutBtn from "@/components/logOutBtn";
 import { Card } from "@/components/ui/card";
-import { useEffect, useState } from "react";
 import {
   Table,
   TableHeader,
@@ -14,19 +14,6 @@ import {
 } from "@/components/ui/table";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-
-interface Session {
-  user: {
-    id: string;
-    name: string;
-    email: string;
-    lastName: string;
-  };
-}
-
-interface DigitadorOpcionesProps {
-  user: Session;
-}
 
 interface Cliente {
   idCliente: number;
@@ -64,7 +51,11 @@ interface Fondo {
   servicios: Servicio[];
 }
 
-const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
+interface DigitadorOpcionesProps {
+  rol: string; // Solo necesitamos el rol
+}
+
+const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ rol }) => {
   const [isFondo, setIsFondo] = useState<boolean>(false);
   const [isProceso, setIsProceso] = useState<boolean>(false);
   const [fondos, setFondos] = useState<Fondo[]>([]);
@@ -142,21 +133,6 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
     const dateWidth = doc.getTextWidth(dateText);
     doc.text(dateText, pageWidth - dateWidth - 10, 15);
 
-    // const headers = [
-    //   [
-    //     "Planilla",
-    //     "Nombre del Cliente",
-    //     "Sede del cliente",
-    //     "100K",
-    //     "50K",
-    //     "20K",
-    //     "10K",
-    //     "5K",
-    //     "2K",
-    //     "Total Verificado",
-    //   ],
-    // ];
-
     const data = inactiveServices.map((servicio) => [
       servicio.planilla,
       selectedFondo.clientes.map((c) => c.name).join(", "),
@@ -175,38 +151,6 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
       0
     );
     data.push(["", "", "", "", "", "", "", "", "Total:", totalSumB.toString()]);
-
-    // doc.autoTable({
-    //   head: headers,
-    //   body: data,
-    //   startY: 25,
-    //   theme: "grid",
-    //   headStyles: {
-    //     fillColor: [8, 145, 178],
-    //     textColor: [0, 0, 0],
-    //     fontStyle: "bold",
-    //   },
-    //   bodyStyles: {
-    //     halign: "center",
-    //   },
-    //   didDrawCell: (data: CellData) => {
-    //     const rowIndex = data.row.index;
-    //     const columnIndex = data.column.index;
-
-    //     if (rowIndex === inactiveServices.length && columnIndex === 8) {
-    //       const cell = data.cell;
-    //       doc.setFont("Helvetica", "bold");
-    //       doc.setDrawColor(0, 0, 0);
-    //       doc.setLineWidth(0.5);
-    //       doc.line(
-    //         cell.x,
-    //         cell.y + cell.height,
-    //         cell.x + cell.width,
-    //         cell.y + cell.height
-    //       );
-    //     }
-    //   },
-    // });
 
     doc.save(
       `Reporte_Servicios_${currentDate}_Fondo_${selectedFondo.nombre}.pdf`
@@ -261,7 +205,7 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
     <div className="min-h-screen bg-gradient-to-bl from-slate-400 to-cyan-800">
       <header className="bg-transparent text-white top-0 z-50 p-6">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-4xl font-bold">Bienvenido, {user.user.name}</h1>
+          <h1 className="text-4xl font-bold">Bienvenido, {rol}</h1>
           <nav>
             <ul className="flex space-x-4">
               <li>
