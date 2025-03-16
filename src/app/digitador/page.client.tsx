3dc70b8c8c200610ce98dcc4b45a3259b0330e29
Nate <6814natsu@gmail.com>
2025-03-16 02:42:34 -0500

    correcciones para el deploy

--- src/app/digitador/page.client.tsx
+++ src/app/digitador/page.client.tsx
@@ -4,8 +4,14 @@ import { Button } from "@/components/ui/button";
 import LogOutBtn from "@/components/logOutBtn";
 import { Card } from "@/components/ui/card";
 import { useEffect, useState } from "react";
-import { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from "@/components/ui/table";
-import { DateTime } from "next-auth/providers/kakao";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "@/components/ui/table";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 
@@ -21,8 +27,6 @@ interface Session {
 interface DigitadorOpcionesProps {
   user: Session;
 }
-
-// types/fondo.ts
 
 interface Cliente {
   idCliente: number;
@@ -41,14 +45,14 @@ interface Servicio {
   planilla: number;
   sello: number;
   estado: "Activo" | "Inactivo";
-  fecharegistro: DateTime;
+  fecharegistro: Date;
   Sum_B: number;
-  B_100000: number,
-  B_50000: number,
-  B_20000: number,
-  B_10000: number,
-  B_5000: number,
-  B_2000: number,
+  B_100000: number;
+  B_50000: number;
+  B_20000: number;
+  B_10000: number;
+  B_5000: number;
+  B_2000: number;
 }
 
 interface Fondo {
@@ -60,70 +64,63 @@ interface Fondo {
   servicios: Servicio[];
 }
 
-
 const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
-
-  const [isFondo, setIsFondo] = useState<Boolean>(false);
-  const [isProceso, setIsProceso] = useState<Boolean>(false);
+  const [isFondo, setIsFondo] = useState<boolean>(false);
+  const [isProceso, setIsProceso] = useState<boolean>(false);
   const [fondos, setFondos] = useState<Fondo[]>([]);
-  const [calculo, setCalculo] = useState<Boolean>(false);
-  const [selectedFondoId, setSelectedFondoId] = useState<number | null>(null); // Fondo seleccionado
-  const [inactiveDates, setInactiveDates] = useState<string[]>([]); // Fechas inactivas del fondo seleccionado
-  const [sumResult, setSumResult] = useState<number | null>(null); // Almacena la suma calculada
+  const [calculo, setCalculo] = useState<boolean>(false);
+  const [selectedFondoId, setSelectedFondoId] = useState<number | null>(null);
+  const [inactiveDates, setInactiveDates] = useState<string[]>([]);
+  const [sumResult, setSumResult] = useState<number | null>(null);
 
-  // Manejar cálculo de la suma
   const handleCalcular = () => {
     if (selectedFondoId === null) return;
 
-    const selectedFondo = fondos.find((fond) => fond.idFondo === selectedFondoId);
+    const selectedFondo = fondos.find(
+      (fond) => fond.idFondo === selectedFondoId
+    );
     if (selectedFondo) {
       const suma = selectedFondo.servicios
         .filter((servicio) => servicio.estado === "Inactivo")
-        .reduce((acc, servicio) => acc + servicio.Sum_B, 0); // Calcula la suma de Sum_B
+        .reduce((acc, servicio) => acc + servicio.Sum_B, 0);
       setSumResult(suma);
     }
 
-    setCalculo(prevState => !prevState);
-
+    setCalculo((prevState) => !prevState);
   };
 
-  function showProceso() {
-    if (isFondo) {
-      setIsFondo(false)
-    }
-    setIsProceso(prevState => !prevState);
-  }
+  const showProceso = () => {
+    if (isFondo) setIsFondo(false);
+    setIsProceso((prevState) => !prevState);
+  };
 
-  function showFondo() {
-    if (isProceso) {
-      setIsProceso(false)
-    }
-    setIsFondo(prevState => !prevState); // Alterna entre true y false
-  }
+  const showFondo = () => {
+    if (isProceso) setIsProceso(false);
+    setIsFondo((prevState) => !prevState);
+  };
 
   const abrirRuta = () => {
-    window.open('/checkin', '_blank');
+    window.open("/checkin", "_blank");
   };
 
-  function onlyUnique(value: any, index: any, self: any) {
+  const onlyUnique = (value: string, index: number, self: string[]) => {
     return self.indexOf(value) === index;
-  }
+  };
 
-  function handlePDF() {
+  const handlePDF = () => {
     if (!fondos.length || !selectedFondoId) {
       alert("No hay datos disponibles para generar el PDF.");
       return;
     }
 
-    // Buscar el fondo seleccionado
-    const selectedFondo = fondos.find((fond) => fond.idFondo === selectedFondoId);
-
+    const selectedFondo = fondos.find(
+      (fond) => fond.idFondo === selectedFondoId
+    );
     if (!selectedFondo) {
       alert("Fondo seleccionado no encontrado.");
       return;
     }
 
-    // Filtrar solo los servicios inactivos del fondo seleccionado
     const inactiveServices = selectedFondo.servicios.filter(
       (servicio) => servicio.estado === "Inactivo"
     );
@@ -133,32 +130,33 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
       return;
     }
 
-    // Crear el documento PDF
     const doc = new jsPDF({ orientation: "landscape" });
-
-    // Obtener fecha actual
     const currentDate = new Date().toLocaleDateString("es-ES");
-
-    // Configuración para centrar el título
     const pageWidth = doc.internal.pageSize.getWidth();
     const title = `Reporte de Servicios Transportadora De Valores - Fondo: ${selectedFondo.nombre} `;
     const titleWidth = doc.getTextWidth(title);
-
-    // Añadir el título centrado
     const dateText = `Fecha: ${currentDate}`;
+
     doc.setFontSize(16);
     doc.text(title, (pageWidth - titleWidth) / 2, 15);
-
-    // Añadir fecha en el encabezado
     const dateWidth = doc.getTextWidth(dateText);
     doc.text(dateText, pageWidth - dateWidth - 10, 15);
 
-    // Encabezado de la tabla
-    const headers = [
-      ["Planilla", "Nombre del Cliente", "Sede del cliente", "100K", "50K", "20K", "10K", "5K", "2K", "Total Verificado"],
-    ];
+    // const headers = [
+    //   [
+    //     "Planilla",
+    //     "Nombre del Cliente",
+    //     "Sede del cliente",
+    //     "100K",
+    //     "50K",
+    //     "20K",
+    //     "10K",
+    //     "5K",
+    //     "2K",
+    //     "Total Verificado",
+    //   ],
+    // ];
 
-    // Datos para la tabla
     const data = inactiveServices.map((servicio) => [
       servicio.planilla,
       selectedFondo.clientes.map((c) => c.name).join(", "),
@@ -172,72 +170,66 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
       servicio.Sum_B,
     ]);
 
-    // Calcular el total de la columna Sum_B
-    const totalSumB = inactiveServices.reduce((acc, servicio) => acc + servicio.Sum_B, 0);
+    const totalSumB = inactiveServices.reduce(
+      (acc, servicio) => acc + servicio.Sum_B,
+      0
+    );
+    data.push(["", "", "", "", "", "", "", "", "Total:", totalSumB.toString()]);
 
-    // Añadir fila con el total al final
-    data.push([
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "Total:",
-      totalSumB.toString(),
-    ]);
+    // doc.autoTable({
+    //   head: headers,
+    //   body: data,
+    //   startY: 25,
+    //   theme: "grid",
+    //   headStyles: {
+    //     fillColor: [8, 145, 178],
+    //     textColor: [0, 0, 0],
+    //     fontStyle: "bold",
+    //   },
+    //   bodyStyles: {
+    //     halign: "center",
+    //   },
+    //   didDrawCell: (data: CellData) => {
+    //     const rowIndex = data.row.index;
+    //     const columnIndex = data.column.index;
 
-    // Usar autoTable para renderizar la tabla
-    doc.autoTable({
-      head: headers,
-      body: data,
-      startY: 25,
-      theme: "grid",
-      headStyles: {
-        fillColor: [8, 145, 178], // Cian
-        textColor: [0, 0, 0],    // Negro
-        fontStyle: "bold",
-      },
-      bodyStyles: {
-        halign: "center", // Centrar texto en celdas
-      },
-      didDrawCell: (data: { row: { index: any; }; column: { index: any; }; cell: any; }) => {
-        const rowIndex = data.row.index;
-        const columnIndex = data.column.index;
+    //     if (rowIndex === inactiveServices.length && columnIndex === 8) {
+    //       const cell = data.cell;
+    //       doc.setFont("Helvetica", "bold");
+    //       doc.setDrawColor(0, 0, 0);
+    //       doc.setLineWidth(0.5);
+    //       doc.line(
+    //         cell.x,
+    //         cell.y + cell.height,
+    //         cell.x + cell.width,
+    //         cell.y + cell.height
+    //       );
+    //     }
+    //   },
+    // });
 
-        // Verificar si es la celda del total (última fila y columna Sum_B)
-        if (rowIndex === inactiveServices.length && columnIndex === 8) {
-          const cell = data.cell;
-          doc.setFont("Helvetica", "bold");
-          doc.setDrawColor(0, 0, 0);
-          doc.setLineWidth(0.5);
-          doc.line(cell.x, cell.y + cell.height, cell.x + cell.width, cell.y + cell.height); // Subrayado
-        }
-      },
-    });
-
-    // Descargar el PDF
-    doc.save(`Reporte_Servicios_${currentDate}_Fondo_${selectedFondo.nombre}.pdf`);
-  }
-
+    doc.save(
+      `Reporte_Servicios_${currentDate}_Fondo_${selectedFondo.nombre}.pdf`
+    );
+    alert("PDF generado y descargado correctamente.");
+  };
 
   const handleFondoChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const fondoId = parseInt(e.target.value); // Obtén el ID del fondo seleccionado
+    const fondoId = parseInt(e.target.value);
     setSelectedFondoId(fondoId);
 
-    // Busca el fondo seleccionado y actualiza las fechas inactivas
     const selectedFondo = fondos.find((fond) => fond.idFondo === fondoId);
     if (selectedFondo) {
-      let fechas = selectedFondo.servicios
-        .filter((servicio) => servicio.estado === "Inactivo") // Solo servicios inactivos
-        .map((servicio) => new Date(servicio.fecharegistro).toLocaleDateString()); // Convierte fechas al formato adecuado
+      const fechas = selectedFondo.servicios
+        .filter((servicio) => servicio.estado === "Inactivo")
+        .map((servicio) =>
+          new Date(servicio.fecharegistro).toLocaleDateString()
+        );
 
-      const fechasC = fechas.filter(onlyUnique)
+      const fechasC = fechas.filter(onlyUnique);
       setInactiveDates(fechasC);
     } else {
-      setInactiveDates([]); // Si no hay fondo seleccionado, resetea las fechas
+      setInactiveDates([]);
     }
   };
 
@@ -245,20 +237,18 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
     console.log("Fecha seleccionada:", e.target.value);
   };
 
-  const handleSubmit = () => {
-
-  }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Lógica para manejar el envío del formulario
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-
-        // Fetch fondos
         const fondosRes = await fetch("/api/fondos");
         if (!fondosRes.ok) throw new Error("Error al cargar fondos");
         const fondosData = await fondosRes.json();
         setFondos(fondosData);
-
       } catch (err) {
         console.error("Error al cargar los datos:", err);
       }
@@ -266,7 +256,6 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
 
     fetchData();
   }, []);
-
 
   return (
     <div className="min-h-screen bg-gradient-to-bl from-slate-400 to-cyan-800">
@@ -291,19 +280,24 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
             <div className="items-center gap-4">
               <Button
                 onClick={abrirRuta}
-                className="w-full max-w-xs py-3 text-white bg-cyan-700 hover:bg-cyan-900 rounded-md">Ver llegadas</Button>
+                className="w-full max-w-xs py-3 text-white bg-cyan-700 hover:bg-cyan-900 rounded-md"
+              >
+                Ver llegadas
+              </Button>
             </div>
             <div className="items-center gap-4">
               <Button
                 onClick={showFondo}
-                className="w-full max-w-xs py-3 text-white bg-cyan-700 hover:bg-cyan-900 rounded-md">
+                className="w-full max-w-xs py-3 text-white bg-cyan-700 hover:bg-cyan-900 rounded-md"
+              >
                 Ver Fondos
               </Button>
             </div>
             <div className="items-center gap-4">
               <Button
                 onClick={showProceso}
-                className="w-full max-w-xs py-3 text-white bg-cyan-700 hover:bg-cyan-900 rounded-md">
+                className="w-full max-w-xs py-3 text-white bg-cyan-700 hover:bg-cyan-900 rounded-md"
+              >
                 Proceso de Cierre de Fecha
               </Button>
             </div>
@@ -355,7 +349,9 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
               </h2>
               <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                 <div>
-                  <label className="block text-sm font-medium text-gray-600">Fondo</label>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Fondo
+                  </label>
                   <select
                     name="fondos"
                     value={selectedFondoId || ""}
@@ -373,10 +369,12 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
                 </div>
 
                 <div>
-                  <label className="block text-sm font-medium text-gray-600">Fechas a cerrar</label>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Fechas a cerrar
+                  </label>
                   <select
                     name="fecha"
-                    disabled={!selectedFondoId} // Deshabilita si no hay fondo seleccionado
+                    disabled={!selectedFondoId}
                     onChange={handleDateChange}
                     className="w-full px-3 py-2 mt-1 border rounded"
                     required
@@ -392,7 +390,9 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
                 <div>
                   <Button
                     type="button"
-                    onClick={handleCalcular} className="bg-cyan-700 hover:bg-cyan-900">
+                    onClick={handleCalcular}
+                    className="bg-cyan-700 hover:bg-cyan-900"
+                  >
                     Calcular
                   </Button>
                 </div>
@@ -406,21 +406,24 @@ const DigitadorOpciones: React.FC<DigitadorOpcionesProps> = ({ user }) => {
             <h2 className="text-lg font-bold text-gray-800">
               Resultado del cálculo
             </h2>
-            <p className="text-gray-600">Suma total de servicios inactivos: {sumResult}</p>
+            <p className="text-gray-600">
+              Suma total de servicios inactivos: {sumResult}
+            </p>
             <br />
             <div>
               <Button
                 type="button"
-                onClick={handlePDF} className="bg-cyan-700 hover:bg-cyan-900">
+                onClick={handlePDF}
+                className="bg-cyan-700 hover:bg-cyan-900"
+              >
                 Crear PDF
               </Button>
             </div>
           </Card>
         )}
-
       </main>
     </div>
   );
-}
+};
 
 export default DigitadorOpciones;
